e8cfaef7359b7ed6cdc8c9f940e932414644531b
Krailas Rattanachai <rkrailas@gmail.com>
2023-11-17 17:48:29 +0700

    first commit

---
+++ odoo-16.0/custom_addons/school_management/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'School Management',
+    'summary': 'School Management Software',
+    'sequence': 10,
+    'description': """
+School Management
+====================
+The specific and easy-to-use Invoicing system in Odoo allows you to keep track of your accounting, even when you are not an accountant. It provides an easy way to follow up on your vendors and customers.
+
+You could use this simplified accounting in case you work with an (external) account to keep your books, and you still want to keep track of payments. This module also offers you an easy method of registering payments, without having to encode complete abstracts of account.
+    """,
+    'category': 'Tools',
+    'version': '16.0.1.0.0',
+    'website': 'https://www.yourwebsite.com/',
+    'depends': ['base', 'contacts', 'hr', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/school.xml',
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
